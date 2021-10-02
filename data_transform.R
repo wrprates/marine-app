28c1ab6f65e9cfe21e8d6e95eca150f13d419ee8
wrprates <wrprates@yahoo.com>
2021-10-02 03:26:36 -0300

    data transform OK, csv uploading

--- data_transform.R
+++ data_transform.R
@@ -8,14 +8,38 @@ dfs <- list()
 dfs$raw <- readr::read_csv(file = base::paste0(project_root_path,"/data/raw/ships.csv"))
 
 # Cleaning data
-# dfs$clean <- 
-dfs$raw %>% 
+dfs$clean <-
+# st = Sys.time()
+  dfs$raw %>% 
+  dplyr::mutate(SHIP_ID = base::as.character(SHIP_ID)) %>% 
+  # head(10000) %>% 
   dplyr::group_by(SHIP_ID) %>% 
   dplyr::arrange(DATETIME, .by_group = TRUE) %>% 
-  # Find the observation when it sailed the longest distance between two consecutive observations
   dplyr::mutate(LAT_lag = dplyr::lag(LAT),
                 LON_lag = dplyr::lag(LON)) %>% 
-  dplyr::mutate(vessel_distance)
+  
+  # First Distance Calculation - SLOW
+  # dplyr::rowwise() %>%
+  # dplyr::mutate(vessel_distance = geosphere::distm(x = c(LON, LAT), y = c(LON_lag, LAT_lag), fun=distHaversine)) #%>%
+  
+  # Second Distance Calculation - FAST, same result.
+  dplyr::mutate(vessel_distance = earth.dist(LON, LAT, LON_lag, LAT_lag)*1000) %>% # Same result of distm, but much faster.
+  
+  # Find the observation when it sailed the longest distance between two consecutive observations
+  dplyr::slice_max(vessel_distance) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::group_by(SHIP_ID, vessel_distance) %>% 
+  
   # If there is a situation when a vessel moves exactly the same amount of meters, please select the most recent
+  dplyr::slice_max(DATETIME) %>% 
+  dplyr::ungroup() 
 
+# end = Sys.time()
+# end-st
+  
+# Write clean CSV
+readr::write_csv(dfs$clean, file = base::paste0(project_root_path,"/data/clean/df_ship_clean.csv") )
+
+
+  
   
