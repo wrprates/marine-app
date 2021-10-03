605e19b9b5ac4ec2cfc43998979f95a488bba9bb
wrprates <wrprates@yahoo.com>
2021-10-03 02:14:27 -0300

    added points to app

--- app/server.R
+++ app/server.R
@@ -4,35 +4,17 @@ server <- function(input, output, session) {
   
   dfs$clean <- readr::read_rds("https://gitlab.com/wrprates/marine-app/-/raw/main/data/clean/df_ship_clean.RDS") 
    
-  
+  df_filtered <- dfs$clean %>% head(10)
+    
   warsaw <- list(lon = 21.0122, lat = 52.2297)
   
-  airly_api_url <- glue("https://airapi.airly.eu/v2/measurements/point?lat={warsaw$lat}&lng={warsaw$lon}")
-  r <- GET(airly_api_url, add_headers(Accept = "application/json", apikey = consts$airly_api_key))
-  
-  # Optimistic response parsing (hackathon mode)
-  airly_data <- content(r)
-  
-  # air_status <- airly_data$current$indexes[[1]]$description
-  pm25_perc <- airly_data$current$standards[[1]]$percent %>% round
-  pm10_perc <- airly_data$current$standards[[2]]$percent %>% round
-  
-  pollution_history <- data.frame(pollutant = c(), measurement = c(), time = c())
-
-  for (i in 1:length(airly_data$history)) {
-    data <- airly_data$history[[i]]
-    date <- anytime(data$fromDateTime)
-    x_date <- glue("{day(date)} ({hour(date)}:00)")
-    new_slice <- data.frame(
-      pollutant = c(data$values[[2]]$name, data$values[[3]]$name), 
-      measurement = c(data$values[[2]]$value, data$values[[3]]$value), 
-      time = c(x_date, x_date)
-    )
-    pollution_history <- rbind(pollution_history, new_slice)
-  }
   
   selected_point <- reactiveValues(id = NULL)
 
+  # output$gaugePM25 <- renderHighchart({ gauge(pm25_perc) })
+  # 
+  # output$gaugePM10 <- renderHighchart({ gauge(pm10_perc) })
+  
   # output$welcomeModal <- renderUI({
   #   create_modal(modal(
   #     id = "simple-modal",
@@ -80,25 +62,57 @@ server <- function(input, output, session) {
       iconAnchorX = 22, iconAnchorY = 94
     )
     
-    random_points <- list(
-      longitudes = warsaw$lon + rnorm(points_n, sd = 0.1),
-      latitudes = warsaw$lat + rnorm(points_n, sd = 0.1)
-    )
+    # random_points <- list(
+    #   longitudes = warsaw$lon + rnorm(points_n, sd = 0.1),
+    #   latitudes = warsaw$lat + rnorm(points_n, sd = 0.1)
+    # )
     
     leaflet() %>% addTiles() %>%
       # addMapboxGL(style = "mapbox://styles/mapbox/streets-v9") %>%
-      setView(lng = warsaw$lon, lat = warsaw$lat, zoom = 12) %>%
-      addEasyButton(easyButton(
-        icon="fa-globe", title="Zoom to Level 1",
-        onClick=JS("function(btn, map){ map.setZoom(1); }"))) %>%
-      addEasyButton(easyButton(
-        icon="fa-crosshairs", title="Locate Me",
-        onClick=JS("function(btn, map){ map.locate({setView: true}); }"))) %>%
-      addMarkers(
-        data = cbind(random_points$longitudes, random_points$latitudes), 
-        layerId = 1:points_n, # marker unique ID numbers
-        icon = smokeIcon
+      # setView(lng = warsaw$lon, lat = warsaw$lat, zoom = 12) %>%
+      # addEasyButton(easyButton(
+      #   icon="fa-globe", title="Zoom to Level 1",
+      #   onClick=JS("function(btn, map){ map.setZoom(1); }"))) %>%
+      # addEasyButton(easyButton(
+      #   icon="fa-crosshairs", title="Locate Me",
+      #   onClick=JS("function(btn, map){ map.locate({setView: true}); }"))) %>%
+      addCircleMarkers(
+        # label = ~ "Start",
+        lng =  df_filtered$LON_lag,
+        lat = df_filtered$LAT_lag,
+        fillColor = "green",
+        fillOpacity = .5,
+        stroke = F
+      ) %>% 
+      
+      addCircleMarkers(
+        # label = ~ "End",
+        lng =  df_filtered$LON,
+        lat = df_filtered$LAT,
+        fillColor = "red",
+        fillOpacity = .5,
+        stroke = F
+      ) %>% 
+      
+      addRectangles(
+        lng1 = df_filtered$LON_lag,
+        lat1 = df_filtered$LAT_lag,
+        lng2 = df_filtered$LON,
+        lat2 = df_filtered$LAT
       )
+      
+      # addMarkers(
+      #   data = cbind(df_filtered$LON_lag, df_filtered$LAT_lag),
+      #   # data = cbind(random_points$longitudes, random_points$latitudes),
+      #   # layerId = 1:nrow(df_filtered), # marker unique ID numbers
+      #   icon = smokeIcon
+      # ) %>% 
+      # addMarkers(
+      #   data = cbind(df_filtered$LON, df_filtered$LAT)#,
+      #   # data = cbind(random_points$longitudes, random_points$latitudes),
+      #   # layerId = 1:nrow(df_filtered), # marker unique ID numbers
+      #   # icon = smokeIcon
+      # )
   })
   
   observe({
@@ -122,85 +136,56 @@ server <- function(input, output, session) {
       )
   })
   
-  output$pollution_stats <- renderUI({
+  output$marine_stats <- renderUI({
     grid(
       grid_template = grid_template(default = list(
         areas = rbind(
           c("status", "status"),
-          c("gauge1", "gauge2"),
+          c("card1", "card2"),
           c("plot", "plot")
         ),
         cols_width = c("50%", "50%"),
         rows_height = c("80px", "160px", "auto")
       )),
-      area_styles = list(gauge1 = "padding-right: 5px", gauge2 = "padding-left: 5px"),
-      status = div(class = "ui message success",
+      area_styles = list(card1 = "padding-right: 5px", card2 = "padding-left: 5px"),
+      
+      status = div(class = "content",
                    "Interesting message"
                    
                    ),
-      gauge1 = card(
+      
+      card1 = card(
         style = "border-radius: 0; width: 100%; height: 150px; background: #efefef",
         div(class = "content",
-            div(class = "header", style = "margin-bottom: 10px", "PM 2.5μm"),
-            div(class = "description", highchartOutput("gaugePM25"))
+            div(class = "header", style = "margin-bottom: 10px", "Card 1")
         )
       ),
-      gauge2 = card(
+      
+      card2 = card(
         style = "border-radius: 0; width: 100%; height: 150px; background: #efefef",
         div(class = "content",
-            div(class = "header", style = "margin-bottom: 10px", "PM 10μm"),
-            div(class = "description", highchartOutput("gaugePM10"))
+            div(class = "header", style = "margin-bottom: 10px", "Card 2")
         )
       ),
+      
       plot = card(
         style = "border-radius: 0; width: 100%; background: #efefef",
         div(class = "content",
-            div(class = "header", style = "margin-bottom: 10px", "Pollution over time"),
-            div(class = "meta", "Measurements from last 24h"),
-            div(class = "description", style = "margin-top: 10px", highchartOutput("pollution", height = "200px"))
+            div(class = "header", style = "margin-bottom: 10px", "Chart Title"),
+            div(class = "meta", "Chart Subtitle"),
+            div(class = "description", style = "margin-top: 10px", 
+                # highchartOutput("pollution", height = "200px")
+                )
         )
       )
     )
   })
   
-  output$polluter_info <- renderUI({
-    id <- selected_point$id
-    mock_photos_n <- 11
-    mock_comments_n <- sample(1:3, 1)
-    mock_alerts_n <- sample(1:50, 1)
-    
-    comment_row <- function(comment) {
-      div(class = "extra content", 
-        div(class = "ui large transparent left icon input", icon("user"), 
-            div(style = "padding-left: 20px; font-size: 10px", comment))
-      )
-    }
-    
-    div(class = "ui card", style = "border-radius: 0; width: 100%; background: #efefef",
-      div(class = "content", 
-        div(class = "right floated meta", "Added: 2020-12-24"), paste0("Chimney #", id)), 
-      
-      div(class = "image", img(src = paste0("images/", id %% mock_photos_n + 1, ".jpg"), style = "height: 250px")), 
-      
-      div(class = "content", 
-        span(class = "right floated", icon("warning"), paste(mock_alerts_n, "alerts")), 
-        icon("comment"), paste(mock_comments_n, "comments")), 
-        tagList(lapply(sample(random_comments, mock_comments_n), comment_row)),
-        div(class = "extra content", 
-          div(class = "ui large transparent left icon input", 
-            icon("comment"), tags$input(type = "text", placeholder = "Add Comment...")
-          )
-        )
-    )
-  })
+  
   
   output$sidebar <- renderUI({
-    if (is.null(selected_point$id)) {
-      uiOutput("pollution_stats")
-    } else {
-      uiOutput("polluter_info")
-    }
-  })
+    uiOutput("marine_stats")
+    })
   
   gauge <- function(value) {
     col_stops <- data.frame(
@@ -245,14 +230,11 @@ server <- function(input, output, session) {
       hc_size(height = 150)
   }
   
-  output$gaugePM25 <- renderHighchart({ gauge(pm25_perc) })
   
-  output$gaugePM10 <- renderHighchart({ gauge(pm10_perc) })
-  
-  output$pollution <- renderHighchart({
-    pollution_history %>% 
-      hchart('areaspline', hcaes(x = 'time', y = 'measurement', group = "pollutant"))
-  })
+  # output$pollution <- renderHighchart({
+  #   pollution_history %>% 
+  #     hchart('areaspline', hcaes(x = 'time', y = 'measurement', group = "pollutant"))
+  # })
   
   
   # camera_snapshot <- callModule(
@@ -274,9 +256,9 @@ server <- function(input, output, session) {
   # })
   
   output$selection_map <- renderLeaflet({
-    leaflet() %>% addTiles() %>%
+    leaflet::leaflet() %>% leaflet::addTiles() %>%
       # addMapboxGL(style = "mapbox://styles/mapbox/streets-v9") %>%
-      setView(lng = warsaw$lon, lat = warsaw$lat, zoom = 12)
+      leaflet::setView(lng = warsaw$lon, lat = warsaw$lat, zoom = 12)
   })
   
 }
